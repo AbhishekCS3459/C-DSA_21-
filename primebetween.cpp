ae79a93b9073e7d0861ffc6371fb632196c1cc97
Abhishek <abhishekverman3459@gmail.com>
2022-02-01 12:25:03 +0530

    commit message

--- primebetween.cpp
+++ primebetween.cpp
@@ -1,10 +1,11 @@
 // Print all the prime numbers between two given numbers
-#include<iostream>
+#include <iostream>
 using namespace std;
-bool isprime(int num){
+bool isprime(int num)
+{
     for (int i = 2; i < num; i++)
     {
-        if (num%i==0)
+        if (num % i == 0)
         {
             return false;
         }
@@ -12,25 +13,19 @@ bool isprime(int num){
         {
             return false;
         }
-        
-        
     }
-    
 }
-int main(int argc, char const *argv[])
+int main()
 {
-    int a,b;
-    cin>>a>>b;
-    for (int i = a; i < b; i++)
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
     {
         if (isprime(i))
         {
-            cout<<i;
+            cout << i;
         }
-        
     }
-    
+
     return 0;
 }
-
-
